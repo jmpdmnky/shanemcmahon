e9a5a5a6393643e6e86bf0fbc488f17f320a536f
jmpdmnky <hoops@drbunsen.localdomain>
2018-11-13 21:07:15 -0500

    a little more

--- make_sample_data.py
+++ make_sample_data.py
@@ -1,9 +1,12 @@
 
 
 template = [
-        #salary
-        #race
-        #sex
+        50000, #salary
+        0, #white
+        0, #black
+        0, #latino
+        0, #asian
+        0, #male
         #job level
         #tenure
         #
